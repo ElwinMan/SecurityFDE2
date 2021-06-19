493e5b991b0950d115cceb33a6912d32d9035718
ElwinMan <man0008@hz.nl>
2021-06-19 16:11:42 +0200

    test article index

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,19 +1,20 @@
 <x-app-layout>
     <main>
-        <div>
-            <h2><a href="{{ route('articles.create') }}">Create New Article</a></h2>
-        </div>
-        <div class="articles">
-            @foreach($articles as $article)
-                <li>
-                    <h3>
-                        <a href="{{ route('articles.show', $article) }}">
-                            {{ $article->title }}
-                        </a>
-                    </h3>
-                    <hr>
-                    {!! $article->excerpt !!}
-                </li>
-        </div>
+        test
+{{--        <div>--}}
+{{--            <h2><a href="{{ route('articles.create') }}">Create New Article</a></h2>--}}
+{{--        </div>--}}
+{{--        <div class="articles">--}}
+{{--            @foreach($articles as $article)--}}
+{{--                <li>--}}
+{{--                    <h3>--}}
+{{--                        <a href="{{ route('articles.show', $article) }}">--}}
+{{--                            {{ $article->title }}--}}
+{{--                        </a>--}}
+{{--                    </h3>--}}
+{{--                    <hr>--}}
+{{--                    {!! $article->excerpt !!}--}}
+{{--                </li>--}}
+{{--        </div>--}}
     </main>
 </x-app-layout>
