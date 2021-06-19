87a29c44701fb6ddb6be0065932fce95cc07177b
ElwinMan <man0008@hz.nl>
2021-06-19 17:21:37 +0200

    idk

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -9,4 +9,5 @@
     <h3>{{ $article->title }}</h3>
     <p>{{ $article->body }}</a></p>
     <button><a href="/articles/{{$article->id}}/edit">Edit</a></button>
+    <button><a href="/articles">back</a></button>
 </div>
