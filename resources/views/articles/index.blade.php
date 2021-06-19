49e2f1abb2726d7ede602ba3d758a0f9465eb67e
ElwinMan <man0008@hz.nl>
2021-06-19 16:14:29 +0200

    uncommented articles

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,19 +1,19 @@
 <main>
     test
-{{--        <div>--}}
-{{--            <h2><a href="{{ route('articles.create') }}">Create New Article</a></h2>--}}
-{{--        </div>--}}
-{{--        <div class="articles">--}}
-{{--            @foreach($articles as $article)--}}
-{{--                <li>--}}
-{{--                    <h3>--}}
-{{--                        <a href="{{ route('articles.show', $article) }}">--}}
-{{--                            {{ $article->title }}--}}
-{{--                        </a>--}}
-{{--                    </h3>--}}
-{{--                    <hr>--}}
-{{--                    {!! $article->excerpt !!}--}}
-{{--                </li>--}}
-{{--        </div>--}}
+        <div>
+            <h2><a href="{{ route('articles.create') }}">Create New Article</a></h2>
+        </div>
+        <div class="articles">
+            @foreach($articles as $article)
+                <li>
+                    <h3>
+                        <a href="{{ route('articles.show', $article) }}">
+                            {{ $article->title }}
+                        </a>
+                    </h3>
+                    <hr>
+                    {!! $article->excerpt !!}
+                </li>
+        </div>
 </main>
 
