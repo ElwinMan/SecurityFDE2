b1f0e73784f6995d8ab0eb912d8c29797289f15c
ElwinMan <man0008@hz.nl>
2021-06-19 17:14:07 +0200

    index

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -8,8 +8,8 @@
 <button><a href="articles/create" style="text-decoration: none"><span class="material-icons">add</span></a></button><br><br>
 @foreach($articles as $article)
     <div>
-        <h3><a href="/articles/{{ $article->id }}" style="text-decoration:none">articleTitle:<p>{{ $article->Title }}</p></a></h3>
-        <p>{{ $article->Excerpt }}</p>
+        <h3><a href="/articles/{{ $article->id }}" style="text-decoration:none">{{ $article->title }}</a></h3>
+        <p>{{ $article->excerpt }}</p>
         <button><a href="/articles/{{$article->id}}/edit">Edit</a></button>
         <form class="button1" method="POST" action="/articles/{{ $article->id }}">
             @csrf
