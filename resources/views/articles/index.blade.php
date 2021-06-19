271e5e5cc152cae7e85975b922e76061c1f72c1a
ElwinMan <man0008@hz.nl>
2021-06-19 16:19:21 +0200

    new index

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,19 +1,20 @@
-<main>
-    test
-        <div>
-            <h2><a href="{{ route('articles.create') }}">Create New Article</a></h2>
-        </div>
-        <div class="articles">
-            @foreach($articles as $article)
-                <li>
-                    <h3>
-                        <a href="{{ route('articles.show', $article) }}">
-                            {{ $article->title }}
-                        </a>
-                    </h3>
-                    <hr>
-                    {!! $article->excerpt !!}
-                </li>
-        </div>
-</main>
+<head>
+    <title>Articles</title>
+</head>
 
+<h1 class="stopBeingSoDifficult">Articles</h1>
+
+
+<button><a href="articles/create" style="text-decoration: none"><span class="material-icons">add</span></a></button><br><br>
+@foreach($articles as $article)
+    <div>
+        <h3><a href="/articles/{{ $article->id }}" style="text-decoration:none">{{ $article->Title }}</a></h3>
+        <p>{{ $article->Excerpt }}</p>
+        <button><a href="/articles/{{$article->id}}/edit">Edit</a></button>
+        <form class="button1" method="POST" action="/articles/{{ $article->id }}">
+            @csrf
+            @method('DELETE')
+            <button class="button1" type="submit"><span class="material-icons">delete</span></button>
+        </form>
+    </div>
+@endforeach
