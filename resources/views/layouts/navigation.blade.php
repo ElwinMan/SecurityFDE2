9ff8b4adca569938971c5bf9d7d0b9a919a2952f
ElwinMan <man0008@hz.nl>
2021-06-19 15:53:02 +0200

    article navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,7 +15,7 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link :href="/articles" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="/articles" :active="request()->routeIs('articles')">
                         {{ __('Articles') }}
                     </x-nav-link>
                 </div>
